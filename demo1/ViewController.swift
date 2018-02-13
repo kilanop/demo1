1b08e8e0978d6355e8b903a3732f2c1051a8a736
kilanop <kilawid69@gmail.com>
2018-02-13 12:06:59 +0800

    new

--- demo1/ViewController.swift
+++ demo1/ViewController.swift
@@ -8,11 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var money: UITextField!
+    @IBOutlet weak var fee: UILabel!
+    @IBOutlet weak var total: UILabel!
+    @IBOutlet weak var carriageChangeSW: UISwitch!
+    @IBOutlet weak var carriageChangeSL: UISlider!
+    
+    @IBAction func changeMoney(_ sender: Any)
+    {
+        Countsum()
+    }
+    var str:String = "0"
+    @IBAction func changeSW(_ sender: Any)
+    {
+        
+        Countsum()
+        if(carriageChangeSW.isOn)
+        {
+            carriageChangeSL.isEnabled = true
+            carriageChangeSL.value = Float(str)!
+            fee.text = String(Int(carriageChangeSL.value))
+        }
+        else
+        {
+            carriageChangeSL.isEnabled = false
+            str = fee.text!
+            fee.text = String(0)
+            carriageChangeSL.value = 0.0
+        }
+        Countsum()
+    }
+    @IBAction func changeSL(_ sender: UISlider)
+    {
+        fee.text = String(Int(sender.value))
+        Countsum()
+    }
+    
+    func Countsum()
+    {
+        var price =  Int(money.text!)!
+        var myfee = 0
+        if carriageChangeSW.isOn
+        {
+            myfee = Int(fee.text!)!
+        }
+        var sum = price + myfee
+        total.text = String(sum)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Countsum()
     }
 
     override func didReceiveMemoryWarning() {
